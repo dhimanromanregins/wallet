8a098612abeaf364ab746986c309c23519f3ab3d
dhimanromanregins <52999468+dhimanromanregins@users.noreply.github.com>
2022-07-28 13:23:14 +0530

    initaial

--- project/settings.py
+++ project/settings.py
@@ -157,4 +157,3 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE='whitenoise.django.GzipManifestStaticFilesStorage'
